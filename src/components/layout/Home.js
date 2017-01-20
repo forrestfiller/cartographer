391b74b8278948e12f33a3edef9603cffac612ef
Forrest Filler <forrestfiller@users.noreply.github.com>
2017-01-19 22:45:20 -0500

    some minor tweeks for the kids

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -8,28 +8,21 @@ class Home extends Component {
 			<div style={{backgroundColor:'#e0e0e0'}}>
 				<header id="header" style={{padding:0}}>
 					<div className="inner">
-
 						<MapNavigation />
 					</div>
 				</header>
-
 				<div id="main">
 					<section id="one">
 						<div className="row">
 							<div className="8u 12u$(small)">
 								<Posts />
-
 							</div>
-
 							<div className="4u 12u$(small)">
 								<Account />
-
 							</div>
 						</div>
-
 					</section>
 				</div>
-
 			</div>
 		)
 	}
